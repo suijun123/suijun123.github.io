8cafe7190d16e76a37a6c77860c78746848fe7f1
suijun123 <105619868+suijun123@users.noreply.github.com>
2022-05-18 00:49:21 +0800

    ss
    ss

---
+++ src/resources/textures.js
@@ -0,0 +1,85 @@
+let billboardTextures = {};
+// 第三块展板图片地址 2022
+billboardTextures.fundTexture2022wu =
+  '../src/jsm/OKR玄武.png';
+billboardTextures.fundTexture2022que =
+  '../src/jsm/OKR朱雀.png';
+billboardTextures.fundTexture2022hu =
+  '../src/jsm/OKR白虎.png';
+billboardTextures.fundTexture2022long =
+  '../src/jsm/OKR青龙.png';
+
+// 第三块展板图片地址 2021
+billboardTextures.fundTexture2021wu =
+  '../src/jsm/玄武海报.png';
+billboardTextures.fundTexture2021que =
+  '../src/jsm/朱雀海报.png';
+billboardTextures.fundTexture2021hu =
+  '../src/jsm/白虎海报.png';
+billboardTextures.fundTexture2021long =
+  '../src/jsm/青龙海报.png';
+
+
+// 第三块展板图片地址 2020
+billboardTextures.fundTexture2020wu =
+  '../src/jsm/zb2020wu.jpg';
+billboardTextures.fundTexture2020que =
+  '../src/jsm/zb2020que.jpg';
+billboardTextures.fundTexture2020hu =
+  '../src/jsm/zb2020hu.jpg';
+billboardTextures.fundTexture2020long =
+  '../src/jsm/zb2020long.jpg';
+
+let boxTexture = {};
+boxTexture.allSkills = '../src/jsm/2022nbao.png';
+boxTexture.allSkills21 = '../src/jsm/2021nbao.png';
+boxTexture.allSkills20 = '../src/jsm/2020nbao.png';
+
+// 砖块材质
+let stoneTexture = '../src/jsm/stone.png';
+// 展板木头材质
+// let woodTexture = '../src/jsm/woodTexture.jpg';
+let woodTexture = '../src/jsm/sbei.png';
+
+// 文字图片地址
+let inputText = {};
+inputText.terpSolutionsText = '../src/jsm/2022wu.png';
+inputText.bagholderBetsText = '../src/jsm/2022que.png';
+inputText.homeSweetHomeText = '../src/jsm/2022hu.png';
+inputText.longText = '../src/jsm/2022long.png';
+
+inputText.long21Text = '../src/jsm/2021long.png';
+inputText.hu21Text = '../src/jsm/2021hu.png';
+inputText.que21Text = '../src/jsm/2021que.png';
+inputText.wu21Text = '../src/jsm/2021wu.png';
+
+inputText.long20Text = '../src/jsm/2020long.png';
+inputText.hu20Text = '../src/jsm/2020hu.png';
+inputText.que20Text = '../src/jsm/2020que.png';
+inputText.wu20Text = '../src/jsm/2020wu.png';
+
+//SVG
+let SVG = {};
+SVG.reactLogo = '../src/jsm/react-svg.svg';
+
+// 链接的跳转地址
+let URL = {};
+URL.blog =
+  'https://huabyte.com';
+URL.ryanfloyd = 'https://huabyte.com';
+// URL.fund = 'https://github.com/AirHua-byte/FundSpider';
+URL.gitHub = 'https://github.com/AirHua-byte';
+URL.BiliBili = 'https://space.bilibili.com/450443708';
+URL.email = 'https://airhua602@gmail.com';
+URL.music = 'https://music.huabyte.com';
+URL.devTo =
+  'http://wpa.qq.com/msgrd?v=3&uin=3301833942&site=qq&menu=yes"><img border="0" src="http://wpa.qq.com/pa?p=2:3301833942:41';
+
+export {
+  billboardTextures,
+  boxTexture,
+  inputText,
+  URL,
+  stoneTexture,
+  woodTexture,
+};
